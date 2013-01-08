da80ff036491475c580793cdcb701cd5c794910f
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-01-07 17:45:32 -0800

    Cleanup.
    Made default values for DataObject.data empty strings so they can be
    written to
    transports without crashing, like None was doing.
    
    Raise exception when template rendering fails.
    
    Added new StationStatus (untested)
    
    Fixed StationStatus updating

--- pywebdlmon/model.py
+++ pywebdlmon/model.py
@@ -26,16 +26,16 @@ class UnknownStation(Exception): pass
 class DataObject(object):
     def __init__(self, cfg):
         self.template = cfg.templates.get_template(self.template_name)
-        self.data = ObservableDict(html=None, json=None)
+        self.data = ObservableDict(html='', json='')
 
-    # this super should make templates and json
-    # subclasses should connect incoming updates to html/json maker
     def update(self, data):
         try:
             self.data['html'] = self.template.render(data=data)
         except Exception:
             log.err("Error rendering template")
+            # twisted.log.msg munges the formatting; use print instead
             print text_error_template().render()
+            raise
         self.data['json'] = json.dumps(data, indent=4, sort_keys=True)
 
 
@@ -48,37 +48,41 @@ class StationList(DataObject):
 
     def update(self, updated_stations):
         self.stations |= set(updated_stations['dataloggers'].iterkeys())
-        data = {'stations': list(self.stations)}
+        data = {'station_list': list(self.stations)}
         super(StationList, self).update(data)
 
 
-class Station(dict):
-    pass
+class StationStatus(DataObject):
+    template_name = 'station_status.html'
+
+    def update(self, station_status):
+        data = dict(station_status=station_status)
+        super(StationStatus, self).update(data)
 
 
 class InstanceStatus(DataObject):
     template_name = 'instance_status.html'
 
     def __init__(self, *args, **kwargs):
-        # Store individual station statuses in here
+        # Individual station statuses
         self.stations = dict()
-
-        # Store full instance status in here
+        # Full instance status in
         self.status = dict()
-
         super(InstanceStatus, self).__init__(*args, **kwargs)
 
     def update(self, updated_stations):
+        # Do my own update
         updated_stations = updated_stations['dataloggers']
         self.status.update(updated_stations)
-#        for station_name, station_status in updated_stations.iteritems():
-#            try:
-#                station = self.stations[station_name]
-#            except KeyError:
-#                station = self.stations[station_name] = Station()
-#            status.update(station.update(station_status))
         data = dict(instance_status=self.status)
         super(InstanceStatus, self).update(data)
+        # Now update my stations
+        for station_name, station_status in updated_stations.iteritems():
+            try:
+                station = self.stations[station_name]
+            except KeyError:
+                station = self.stations[station_name] = Station()
+            station.update(station_status)
 
     def get_station(self, name):
         try:
@@ -88,8 +92,7 @@ class InstanceStatus(DataObject):
 
 
 class Instance(DataObject):
-    # TODO FIXME
-    template_name = 'index.html'
+    template_name = 'instance.html'
 
     def __init__(self, name, sources, cfg, *args, **kwargs):
         self.name = name
@@ -97,14 +100,15 @@ class Instance(DataObject):
         self.instance_status = InstanceStatus(cfg)
         self.station_list = StationList(cfg)
         for source in sources:
-# TODO Fix async connect packet corruption issue
+# TODO Fix async connect packet corruption issue.
 #            def on_connect(r):
 #                self.reap(source)
 #            d = source.connect()
 #            d.addCallback(on_connect)
             log.msg("connecting to src %r" % source)
-            # NOTE Using syncronous connect until we fix async connect
+            # NOTE Using sync connect until we fix async connect
             kudu.orb.Orb.connect(source)
+            # NOTE this is handy for debugging but maybe not for production
             source.seek(kudu.orb.ORBOLDEST)
             self.reap(source)
         super(Instance, self).__init__(cfg, *args, **kwargs)
@@ -129,14 +133,15 @@ class Instance(DataObject):
         self.instance_status.update(updated_stations)
         self.station_list.update(updated_stations)
         return
-        # NOTE not sure yet what if any data instance should export
+        # NOTE not sure yet what if any data instance should export. Probably
+        # none. Some metadata would be handy though.
         data = dict()
         data['name'] = self.name
         super(Instance, self).update(data)
 
 
 class InstanceCollection(object):
-    """A collection of dlmon instances."""
+    # TODO this should export a list of instances.
     def __init__(self, cfg):
         instances = self.instances = {}
         for name, srcs in cfg.instances.iteritems():
